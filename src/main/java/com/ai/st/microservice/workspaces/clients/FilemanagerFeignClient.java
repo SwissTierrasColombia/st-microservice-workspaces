65d3a8e19e1e858bc4cdc4bcd0a0fc88a7a4fc09
Felipe Cano <felipecanol@gmail.com>
2019-11-26 13:41:06 -0500

    Updated Filemanager FeignClient to support byte[] transfer files

--- src/main/java/com/ai/st/microservice/workspaces/clients/FilemanagerFeignClient.java
+++ src/main/java/com/ai/st/microservice/workspaces/clients/FilemanagerFeignClient.java
@@ -2,19 +2,15 @@ package com.ai.st.microservice.workspaces.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-import org.springframework.http.MediaType;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.ai.st.microservice.workspaces.dto.managers.ManagerDto;
 
 @FeignClient(name = "st-microservice-filemanager", configuration = { FeignSupportConfig.class })
 public interface FilemanagerFeignClient {
 
-	@RequestMapping(value = "/api/filemanager/v1/file", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ManagerDto saveFile(@RequestParam(value = "file", name = "file") MultipartFile file,
-			@RequestParam("driver") String driver);
+	@PostMapping(value = "/api/filemanager/v1/filebytes")
+	public ManagerDto saveFile(@RequestParam(value = "file", name = "file") byte[] file,
+			@RequestParam("filename") String filename, @RequestParam("driver") String driver);
 
 }
